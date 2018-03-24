4fe1663704ce77eb10f1c5f8cfb4caccd0df64d7
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2018-03-24 23:39:03 +0530

    initial

--- app/src/main/java/com/mywings/coderepackging/JavaShowActivity.java
+++ app/src/main/java/com/mywings/coderepackging/JavaShowActivity.java
@@ -1,13 +1,34 @@
 package com.mywings.coderepackging;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class JavaShowActivity extends AppCompatActivity {
+
+    private Button btnPlace1;
+    private Button btnPlace2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_show);
+        btnPlace1 = findViewById(R.id.btnBrowse1);
+        btnPlace2 = findViewById(R.id.btnBrowse2);
+        btnPlace1.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            startActivityForResult(intent,10090);
+
+        });
+        btnPlace2.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            startActivityForResult(intent,1000);
+
+        });
     }
 }
